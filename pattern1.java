e21e52f6a90d1380e95eb15b05acb3da797a37f7
B M SHUBHANK <bmshubhank8210@gmail.com>
2026-08-16 14:31:25 +0530

    Solid Rectangle

---
+++ pattern1.java
@@ -0,0 +1,11 @@
+public class pattern1{
+    public static void main(String[] args){
+        int n=5,m=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
