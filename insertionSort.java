9f674a551b279856c641d6598db6de80f1619b54
B M SHUBHANK <bmshubhank8210@gmail.com>
2026-08-17 18:21:59 +0530

    Insretion Sort

---
+++ insertionSort.java
@@ -0,0 +1,16 @@
+public class insertionSort {
+    public static void main(String[] args){
+        int arr[]={5,2,7,9,1};
+        for(int i=1;i<arr.length;i++){
+            int current=arr[i];
+            int j=i-1;
+            while(j>=0 && current<arr[j]){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=current;
+        }
+        for(int i=0;i<arr.length;i++)
+            System.out.print(arr[i]+" ");
+    }    
+}
