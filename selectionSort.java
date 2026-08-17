d254be7c59ec982c1ce8911f55e6301e2599e634
B M SHUBHANK <bmshubhank8210@gmail.com>
2026-08-17 10:24:25 +0530

    Selection sort

---
+++ selectionSort.java
@@ -0,0 +1,19 @@
+public class selectionSort {
+    public static void main(String[] args){
+        int arr[]={4,6,1,3,8};
+        for(int i=0;i<arr.length-1;i++){
+            int small=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[small]>arr[j]){
+                    small=j;
+                }
+            }
+            int temp=arr[small];
+            arr[small]=arr[i];
+            arr[i]=temp;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
