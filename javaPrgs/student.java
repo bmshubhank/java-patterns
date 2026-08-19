de445b0fba3a3873321d2464fb937341871b6b29
B M SHUBHANK <bmshubhank8210@gmail.com>
2026-08-19 21:33:36 +0530

    Student details

--- javaPrgs/student.java
+++ javaPrgs/student.java
@@ -1,5 +1,28 @@
 //package java-patterns.javaPrgs;
-
+import java.util.*;
 public class student {
-    
+    int roll;
+    String name;
+    void display(){
+        System.out.println("Name:"+name);
+        System.out.println("ROll No:"+roll);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        student s1=new student();
+        student s2=new student();
+
+        s1.name=sc.nextLine();
+        s1.roll=sc.nextInt();
+        sc.nextLine();
+
+        s2.name=sc.nextLine();
+        s2.roll=sc.nextInt();
+        sc.nextLine();
+
+        s1.display();
+        s2.display();
+
+        sc.close();
+    }
 }
